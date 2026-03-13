e5597cc2fb99582c7402c3f19625449174eb8daf
Cukurikik <ncsremixindonesia@gmail.com>
2026-03-13 22:45:07 +0700

    fix: added missing provideAnimationsAsync provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import {
   isDevMode
 } from '@angular/core';
 import {provideRouter, withPreloading, PreloadAllModules} from '@angular/router';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideStore} from '@ngrx/store';
 import {provideEffects} from '@ngrx/effects';
 import {provideStoreDevtools} from '@ngrx/store-devtools';
@@ -16,6 +17,7 @@ import {TrimmerEffects} from './modules/video/01-trimmer/trimmer.effects';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideAnimationsAsync(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideStore({
       [trimmerFeature.name]: trimmerFeature.reducer
